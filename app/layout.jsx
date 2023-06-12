dfce0eca28e64620f037e20b7b721236f62dd783
Amanda Pond <thatotherpond@gmail.com>
2023-06-12 13:22:13 -0400

    Added google authorization

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import Nav from '@components/Nav'
 import Provider from '@components/Provider'
 
 export const metadata = {
-    title: "Promptomatic",
+    title: "Promptomania",
     description: "Discover & Share AI Prompts"
 }
 
@@ -11,13 +11,15 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
         <body>
-            <div className='main'>
-                <div className='gradient' />
-            </div>
-            <main className='app'>
-                <Nav />
-                {children}
-            </main>
+            <Provider>
+                <div className='main'>
+                    <div className='gradient' />
+                </div>
+                <main className='app'>
+                    <Nav />
+                    {children}
+                </main>
+            </Provider>
         </body>
     </html>
   )
